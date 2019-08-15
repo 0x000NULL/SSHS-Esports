df696ee048984e6e3b0ef33558380c298972de73
Ethan Aldrich <1041177@ad.ccsd.net>
2019-08-15 11:55:53 -0700

    Aded Leauge info page

---
+++ routes/leaugeinfo.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+/* GET about page. */
+router.get('/', function (req, res, next) {
+    res.render('leaugeinfo', { title: 'SSHS Esports', page: 'leaugeinfo' });
+});
+module.exports = router;
