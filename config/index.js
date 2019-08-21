5b6d3c7bec99ffabb5d04d5a886a3bcf993c6301
ethanaldrich9986 <ethanaldrich9986@gmail.com>
2019-08-20 18:47:59 -0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-module.exports = {
-  port: 3000 || process.env.PORT,
-  engine: 'ejs',
-  db: 'mongodb://localhost:27017/blog'
-}
+//module.exports = {
+//  port: 3000 || process.env.PORT,
+//  engine: 'ejs',
+//  db: 'mongodb://localhost:27017/blog'
+//}
