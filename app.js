30302c43c7180ba7f4fe79578c1b2d72b5f2ec79
Ethan Aldrich <ethanaldrich9986@gmail.com>
2019-09-13 18:15:30 -0700

    and I am going to kill my self.

--- app.js
+++ app.js
@@ -112,9 +112,6 @@ app.use(function (err, req, res, next) {
 var httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-var httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
 //httpServer.listen(80);
 //httpsServer.listen(443);
 app.set('port', process.env.PORT || 80);
